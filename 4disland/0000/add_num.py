bf9bf91e6550e511468eee2e4f3a97699d6d2895
Poly Bui <141519476+Polybui92@users.noreply.github.com>
2023-11-30 19:47:20 -0600

    Update add_num.py

--- 4disland/0000/add_num.py
+++ 4disland/0000/add_num.py
@@ -12,3 +12,19 @@ def add_num(img):
 if __name__ == '__main__':
     image = Image.open('image.jpg')
     add_num(image)
+## Improve code better 
+
+from PIL import Image, ImageDraw, ImageFont
+
+def add_num(img):
+    draw = ImageDraw.Draw(img)
+    myfont = ImageFont.truetype('C:/windows/fonts/Arial.ttf', size=40)
+    fillcolor = "#ff0000"
+    width, height = img.size
+    draw.text((width-40, 0), '99', font=myfont, fill=fillcolor)
+    img.save('result.jpg','jpeg')
+
+    return 0
+if __name__ == '__main__':
+    image = Image.open('image.jpg')
+    add_num(image)
